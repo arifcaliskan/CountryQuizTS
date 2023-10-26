e18c0f907e28b52fed58c7f154f6d37699cbaf5c
Arif Caliskan <85301866+arifcaliskan@users.noreply.github.com>
2023-10-26 23:18:57 +0300

    Mapping countries work

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
-import axios, { AxiosResponse } from "axios";
+import axios from "axios";
 import { api } from "../api/api";
-import SingleCountry from "./SingleCountry";
+// import SingleCountry from "./SingleCountry";
 
 export type Countries = {
   name: string;
@@ -9,35 +9,23 @@ export type Countries = {
   pop: number;
 };
 const Hero: React.FC = () => {
-  const [countries, setCountries] = useState([] as any[]);
-  const [countriesData, setCountriesData] = useState<Countries[]>();
-  const fetchData = async () => {
-    const response: AxiosResponse = await axios.get(api);
-    setCountries(response.data);
-  };
-  useEffect(() => {
-    fetchData();
+  const [countries, setCountries] = useState<Countries[] | null>(null);
+
+useEffect(() => {
+    axios.get(api).then((response)=>{
+      setCountries(response.data);
+    })
+    countries?console.log(countries):'';
   }, []);
-  useEffect(() => {
-    console.log(countries);
-    if (countries) {
-      for (let i = 0; i < countries?.length; i++) {
-        setCountriesData({
-          ...countriesData,
-          name: countries?.[i]?.name.common,
-          capital: countries?.[i].capital?.[0],
-          pop: countries?.[i]?.population,
-        });
-      }
-    }
-  }, [countries]);
 
   return (
     <div>
       <h1>Countries</h1>
-      {countriesData?.map((c)=>{
-        return (<h2>{c.name}</h2>)
-      })}
+      {countries
+        ? countries.map((c)=>{
+          return <div key={c.pop}>{c.name.common + ' - '+c.capital}</div>
+        }) 
+        : null}
     </div>
   );
 };
